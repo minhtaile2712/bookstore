c6f760813f5ff160cee0554cff61d3f7f5d6b373
minhtaile2712 <minhtaile2712@gmail.com>
2022-08-28 18:52:36 +0700

    Update Book class with Author, update ef core mapping

--- src/Acme.BookStore.EntityFrameworkCore/EntityFrameworkCore/BookStoreDbContext.cs
+++ src/Acme.BookStore.EntityFrameworkCore/EntityFrameworkCore/BookStoreDbContext.cs
@@ -103,6 +103,16 @@ public class BookStoreDbContext :
             b.HasIndex(x => x.Name);
         });
 
+        builder.Entity<Book>(b =>
+        {
+            b.ToTable(BookStoreConsts.DbTablePrefix + "Books", BookStoreConsts.DbSchema);
+            b.ConfigureByConvention(); //auto configure for the base class props
+            b.Property(x => x.Name).IsRequired().HasMaxLength(128);
+
+            // ADD THE MAPPING FOR THE RELATION
+            b.HasOne<Author>().WithMany().HasForeignKey(x => x.AuthorId).IsRequired();
+        });
+
         //builder.Entity<YourEntity>(b =>
         //{
         //    b.ToTable(BookStoreConsts.DbTablePrefix + "YourEntities", BookStoreConsts.DbSchema);
