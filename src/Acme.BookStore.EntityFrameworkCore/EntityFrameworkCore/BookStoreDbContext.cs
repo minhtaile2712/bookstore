83c60c6db2d28684c55199e83024da63981b3db6
minhtaile2712 <minhtaile2712@gmail.com>
2022-11-07 08:52:01 +0700

    dont know

--- src/Acme.BookStore.EntityFrameworkCore/EntityFrameworkCore/BookStoreDbContext.cs
+++ src/Acme.BookStore.EntityFrameworkCore/EntityFrameworkCore/BookStoreDbContext.cs
@@ -81,21 +81,12 @@ public class BookStoreDbContext :
 
         /* Configure your own tables/entities inside here */
 
-        builder.Entity<Book>(b =>
-        {
-            b.ToTable(BookStoreConsts.DbTablePrefix + "Books",
-                BookStoreConsts.DbSchema);
-            b.ConfigureByConvention(); //auto configure for the base class props
-            b.Property(x => x.Name).IsRequired().HasMaxLength(128);
-        });
+
 
         builder.Entity<Author>(b =>
         {
-            b.ToTable(BookStoreConsts.DbTablePrefix + "Authors",
-                BookStoreConsts.DbSchema);
-
+            b.ToTable(BookStoreConsts.DbTablePrefix + "Authors", BookStoreConsts.DbSchema);
             b.ConfigureByConvention();
-
             b.Property(x => x.Name)
                 .IsRequired()
                 .HasMaxLength(AuthorConsts.MaxNameLength);
